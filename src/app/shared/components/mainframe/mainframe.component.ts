4fc4231928b8677e412371e4256af0fe7c942a5a
Serban681 <serbands20@gmail.com>
2023-12-13 14:58:00 +0200

    Added Mainframe component

---
+++ src/app/shared/components/mainframe/mainframe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainframe',
+  templateUrl: './mainframe.component.html',
+  styleUrls: ['./mainframe.component.scss']
+})
+export class MainframeComponent {
+
+}
