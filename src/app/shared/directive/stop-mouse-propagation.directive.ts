675260cc0949bbc132f9b59c503b7eadcc7de260
DanceaRadu <radu.dancea@gmail.com>
2023-12-16 23:44:29 +0200

    #8 Changed stopPropagation directive selector

--- src/app/shared/directive/stop-mouse-propagation.directive.ts
+++ src/app/shared/directive/stop-mouse-propagation.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[clickStopPropagation]',
+  selector: '[appClickStopPropagation]',
 })
 export class StopMousePropagationDirective {
   @HostListener('click', ['$event'])
