29a88f02bba5dd6aeadd3d2629a94a71454b2f60
Serban681 <serbands20@gmail.com>
2023-12-27 15:01:59 +0200

    Pull commit

--- src/app/components/create-competition/create-competition.component.ts
+++ src/app/components/create-competition/create-competition.component.ts
@@ -12,6 +12,48 @@ export class CreateCompetitionComponent {
 
   searchValue = '';
 
+  userOptions = [
+    'sarah',
+    'james',
+    'joe',
+    'john',
+    'jane',
+    'jimmy',
+    'jessica',
+    'jennifer',
+    'julie',
+    'jacob',
+  ];
+  selectedUsers: string[] = [];
+
+  userGroupOptions = [
+    'group1',
+    'group2',
+    'group3',
+    'group4',
+    'group5',
+    'group6',
+    'group7',
+    'group8',
+    'group9',
+    'group10',
+  ];
+  selectedUserGroups: string[] = [];
+
+  eventOptions = [
+    'event1',
+    'event2',
+    'event3',
+    'event4',
+    'event5',
+    'event6',
+    'event7',
+    'event8',
+    'event9',
+    'event10',
+  ]
+  selectedEvents: string[] = [];
+
   constructor () {
     this.competitionForm = new FormGroup({
       name: new FormControl(''),
@@ -28,5 +70,17 @@ export class CreateCompetitionComponent {
     this.searchValue = value;
 
     console.log(this.searchValue)
+  }
+
+  handleUserSelect(users: string[]) {
+    this.selectedUsers = users;
+  }
+
+  handleUserGroupSelect(userGroups: string[]) {
+    this.selectedUserGroups = userGroups;
+  }
+
+  handleEventSelect(events: string[]) {
+    this.selectedEvents = events;
   }
 }
