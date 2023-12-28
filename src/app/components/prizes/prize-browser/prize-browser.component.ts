53c14229c6197c792eea5e234bfaad2a5e1ea965
DanceaRadu <radu.dancea@gmail.com>
2023-12-28 19:14:29 +0200

    #23 Created a new component for browsing prizes/extractions.

---
+++ src/app/components/prizes/prize-browser/prize-browser.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prize-browser',
+  templateUrl: './prize-browser.component.html',
+  styleUrls: ['./prize-browser.component.scss']
+})
+export class PrizeBrowserComponent {
+
+}
