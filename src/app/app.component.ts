b2d3d1f233b7f0d7e6b08ec03079a2aa5a6995e8
DanceaRadu <radu.dancea@gmail.com>
2024-01-15 11:30:58 +0200

    Removed the test array from app.component and removed the testing file for the app.component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import { ThemeService } from './service/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private themeService: ThemeService) {}
-
-  options = ['One', 'Two', 'Three'];
-  selectedOptions: string[] = [];
-
-  public toggleTheme() {
-    this.themeService.toggleTheme();
-  }
-
-  public handleSelectedOptions(selectedOptions: string[]) {
-    this.selectedOptions = selectedOptions;
-
-    console.log(this.selectedOptions);
-  }
-}
+export class AppComponent {}
