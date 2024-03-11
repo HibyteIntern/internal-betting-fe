98fc19e423c1e0f0a7cb91f8f9a090bef4b3a226
Eugen <vladionescu511@gmail.com>
2024-03-11 15:18:54 +0200

    added env variables

---
+++ src/assets/env.js
@@ -0,0 +1,7 @@
+// This file is only used in production, values will be replaced with environment variables values
+(function(window) {
+  window["env"] = window["env"] || {};
+
+  // Environment variables
+  window["env"]["baseUrl"] = "${BASE_URL}";
+})(this);
